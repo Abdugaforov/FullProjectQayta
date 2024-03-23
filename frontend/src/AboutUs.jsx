7ba97c9eaeee648133958a85a6d88444450f091e
user <shahzodqurbonovazamatovich@gmail.com>
2024-03-23 17:55:05 +0500

    frontni qitisi

---
+++ frontend/src/AboutUs.jsx
@@ -0,0 +1,23 @@
+import './css/AboutUs.css'
+import picture from "./photos/FmlS61J9LP 1.svg";
+function AboutUs() {
+    return (
+        <div>
+          <div className={"middle"}>
+              <div className={"middle_container"}>
+                  <div className={"middle_picture"}>
+                      <img className={"middle_img"} src={picture} alt=""/>4
+                  </div>
+                  <div className={"middle_left"}>
+                        <p className={"middle_p"}>ABOUT US</p>
+                      <h1 className={"middle_h1"}>Food Stalls with Persons but to Product marketing plane catlogues etc to.</h1>
+                      <p className={"middle_p2"}>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also equipment make your marketing plane Effective.</p>
+                      <button className={"btn_middle"}>READ MORE</button>
+                  </div>
+              </div>
+          </div>
+        </div>
+    )
+}
+
+export default AboutUs
---
+++ frontend/src/Fatures.jsx
@@ -0,0 +1,36 @@
+import './css/Feature.css'
+import  rasm1 from "./photos/Group 94.svg"
+import  rasm2 from "./photos/food-delivery.svg"
+import  rasm3 from "./photos/Group 93.svg"
+function Features() {
+    return (
+        <div className={"feature"}>
+          <div className={"feature_container"}>
+              <h3 className={"text-center feature_text"}>Features</h3>
+              <h1 className={"feature_text2"}>Food with a New Passion</h1>
+              <div className={"box_group"}>
+                  <div className={"box"}>
+                      <div className={"feature_box_img"}>
+                          <img className={"box_img"} src={rasm1} alt=""/>
+                      </div>
+                      <h2 className={"box_h1"}>Quality Food</h2>
+                      <p className={"box_p"}>It can be a very secure path to earn good money and make you very successful creative entrepreneur.</p>
+                  </div>
+                  <div className={"box"}>
+                      <div className={"feature_box_img"}><img className={"box_img"} src={rasm2} alt=""/></div>
+                      <h2 className={"box_h1"}>Food Delivery</h2>
+                      <p className={"box_p"}>It can be a very secure path to earn good money and make you very successful creative entrepreneur.</p>
+                  </div>
+                  <div className={"box"}>
+                      <div className={"feature_box_img"}><img className={"box_img"} src={rasm3} alt=""/></div>
+                      <h2 className={"box_h1"}>Super Taste</h2>
+                      <p className={"box_p"}>It can be a very secure path to earn good money and make you very successful creative entrepreneur.</p>
+                  </div>
+              </div>
+
+          </div>
+        </div>
+    )
+}
+
+export default Features
