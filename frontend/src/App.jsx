bd8276dfd25c9b793faf7f92ce02d38de5bd28a5
Asus vivobook <sardorsanoyev441@gmail.com>
2024-03-23 19:46:21 +0500

    keraksiz narsalarni yoqotish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,7 @@ import picture from "./photos/CSmJnTefxM 1.svg";
 import {useNavigate} from "react-router-dom";
 function App() {
     const x=useNavigate();
-    function homePage() {
 
-    }
 
     return (
         <div>
@@ -16,11 +14,11 @@ function App() {
                              src="https://s3-alpha-sig.figma.com/img/e8ad/f3eb/c77a6232d187d0552a6a4dc4f0b003ae?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VjO~rW7~xKjyglykACouBOniP7vQH9MyS-pzCT0C1x-FktHOKmBZ5vdIfw-WUeApKdPag2KYEg2NpSuMwrdXDb0CfUsKDgInA5kQaF2C4hBU8URddYPyVvjxC0bkAJz-lNNZZc4g8pnjei3VMqbLOVCyBIAyFlWDyjSEvF0TPg8jBSJ6HNo-GXyhhKZsuL450PO1C9YmRu2V75qjsPc9SE7t-033OVS96xCcNPgg25tRQ-tJUjIKUa1nK0ZfNw33xzN4o1mOHujK4qbi-MZnvfEZLcF5TOdaAn4uc6RllJHARTF65-nxJIiguAY0CG1p0KL2INLOuN9ppgkG6~X8Xg__"
                              alt=""/>
                         <ul className={"ul_group"}>
-                            <li onClick={(e)=>x("/")} className={"li1"}>Home</li>
-                            <li onClick={(e)=>x("/aboutUs")} className={"li"}>About us</li>
-                            <li onClick={(e)=>x("/menu")} className={"li"}>Menu</li>
-                            <li onClick={(e)=>x("/features")} className={"li"}>Features</li>
-                            <li onClick={(e)=>x("/")} className={"li"}>contact us</li>
+                            <li onClick={()=>x("/")} className={"li1"}>Home</li>
+                            <li onClick={()=>x("/aboutUs")} className={"li"}>About us</li>
+                            <li onClick={()=>x("/menu")} className={"li"}>Menu</li>
+                            <li onClick={()=>x("/features")} className={"li"}>Features</li>
+                            <li onClick={()=>x("/")} className={"li"}>contact us</li>
                         </ul>
                     </div>
                     <div className={"box2_container"}>
