d2455ae7999dfb4d364fb45f0f6cf64222818a2e
Asus vivobook <sardorsanoyev441@gmail.com>
2024-03-24 22:22:43 +0500

    ...

--- Beckend/src/main/java/com/example/beckend/servise/OrderService.java
+++ Beckend/src/main/java/com/example/beckend/servise/OrderService.java
@@ -1,9 +1,15 @@
 package com.example.beckend.servise;
 
+import com.example.beckend.dto.OrderDto;
 import com.example.beckend.entity.Order;
+import org.springframework.http.HttpEntity;
 
 import java.util.List;
 
 public interface OrderService {
     List<Order>getOrderByProductId(Long productId);
+
+    default HttpEntity<?> saveOrder(OrderDto dto) {
+        return null;
+    }
 }
