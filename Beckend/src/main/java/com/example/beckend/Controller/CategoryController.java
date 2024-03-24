de141fe456065455335f8996e700e1b73be33b61
Asus vivobook <sardorsanoyev441@gmail.com>
2024-03-24 16:21:21 +0500

    update

--- Beckend/src/main/java/com/example/beckend/Controller/CategoryController.java
+++ Beckend/src/main/java/com/example/beckend/Controller/CategoryController.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin
 @RequestMapping("/category")
-@RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
 
