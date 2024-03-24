4e105023c703121ceec1d8886b4abe700304d74c
Asus vivobook <sardorsanoyev441@gmail.com>
2024-03-24 21:50:31 +0500

    ...

--- Beckend/src/main/java/com/example/beckend/servise/impl/OrderImpl.java
+++ Beckend/src/main/java/com/example/beckend/servise/impl/OrderImpl.java
@@ -1,9 +1,11 @@
 package com.example.beckend.servise.impl;
 
+import com.example.beckend.dto.OrderDto;
 import com.example.beckend.entity.Order;
 import com.example.beckend.repo.OrderRepo;
 import com.example.beckend.servise.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +19,10 @@ public class OrderImpl implements OrderService {
     @Override
     public List<Order> getOrderByProductId(Long productId) {
         return orderRepo.findAllByProductId(productId);
+    }
+
+    @Override
+    public HttpEntity<?> saveOrder(OrderDto dto) {
+        return null;
     }
 }
